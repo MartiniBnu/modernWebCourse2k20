538a27f9485c2d09d7697d5b811dd82666ab64b9
Marcos Ernani Martini <marcos.martini@mouts.info>
2021-01-07 23:33:32 -0300

    Novas

--- 3-funcoes/15-tiposDeclaracao.js
+++ 3-funcoes/15-tiposDeclaracao.js
@@ -1,3 +1,10 @@
+// Interpetrador já tem todas function declaration
+console.log(soma(3,4))
+// Function expression e named não dá
+//console.log(sub(3,4))
+//console.log(mult(3,4))
+
+
 // Forma tradicional - function declaration
 function soma(x,y){
   return x + y
@@ -5,10 +12,12 @@ function soma(x,y){
 
 // function expression 
 const sub = function(x,y) {
-  returno x - y
+  return x - y
 }
+console.log(sub(3,4))
 
 // named function expression (só ajuda quando está debugando um stack trace pq mostra o nome interno da função)
 const mult = function mult(x,y) {
   return x * y
 }
+console.log(mult(3,4))
