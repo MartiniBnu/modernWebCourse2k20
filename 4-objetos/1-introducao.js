4241c13702128bc08588c054e3e12418db0db69a
Marcos Ernani Martini <marcos.martini@mouts.info>
2021-01-08 23:41:20 -0300

    exercicios

---
+++ 4-objetos/1-introducao.js
@@ -0,0 +1,17 @@
+// codigo não executável
+
+// procedural
+//processamento (valor1, valor2, valor3)
+
+// OO 
+objeto = {
+  valor1 =1,
+  valor2 =0,
+  valor3 =1,
+  processamento() {
+
+  }
+}
+
+objeto.processamento() // foco desse exemplo passou a ser o objeto
+
