089617f9ff169fbe920c2e6ad8d4ba13bdea64ca
Marcos Martini <martini.bnu@gmail.com>
2021-02-08 22:58:51 -0300

    Final Webpack

--- 13-webpack/src/principal.js
+++ 13-webpack/src/principal.js
@@ -1,5 +1,7 @@
 import Pessoa from './pessoa'
+import './assets' // Quando não tem nada depois, ele irá procurar algum arquivo index.js
 import './modulos/moduloA'
+
 
 const atendente = new Pessoa 
 console.log(atendente.cumprimentar())
